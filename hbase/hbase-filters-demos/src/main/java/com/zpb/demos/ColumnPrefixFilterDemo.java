31a4468a703c9985850d6b5010db8cb1788e964b
zhou.pengbo <zhou.pengbo@xcar.com.cn>
2020-06-03 18:55:10 +0800

    Update ColumnPrefixFilterDemo.java

--- hbase/hbase-filters-demos/src/main/java/com/zpb/demos/ColumnPrefixFilterDemo.java
+++ hbase/hbase-filters-demos/src/main/java/com/zpb/demos/ColumnPrefixFilterDemo.java
@@ -2,24 +2,59 @@ package com.zpb.demos;
 
 import com.zpb.utils.MyBase;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.ColumnPaginationFilter;
+import org.apache.hadoop.hbase.filter.ColumnPrefixFilter;
 import org.apache.hadoop.hbase.io.compress.Compression;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.security.UserGroupInformation;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
 
+/**
+ * 根据列名前缀过滤匹配列数据
+ */
 public class ColumnPrefixFilterDemo {
 
+    private static boolean isok = false;
     private static String tableName = "test";
+    private static String[] cfs = new String[]{"f1","f2"};
+    private static String[] data = new String[]{"row-1:f1:p:v1", "row-1:f1:pre:v2", "row-1:f2:pref:v3", "row-1:f2:prefix:v4"};
     public static void main(String[] args) throws IOException {
+
         MyBase myBase = new MyBase();
         Connection connection = myBase.createConnection();
-        myBase.deleteTable(connection,tableName);
-        myBase.createTable(connection,tableName,"f");
+        if (isok) {
+            myBase.deleteTable(connection, tableName);
+            myBase.createTable(connection, tableName, cfs);
+            // 造数据
+            myBase.putRows(connection, tableName, data);
+        }
+        Table table = connection.getTable(TableName.valueOf(tableName));
+        Scan scan = new Scan();
+        ColumnPrefixFilter columnPrefixFilter = new ColumnPrefixFilter(Bytes.toBytes("pref")); // [row-1:f2:pref, row-1:f2:prefix]
+        scan.setFilter(columnPrefixFilter);
+        ResultScanner scanner = table.getScanner(scan);
+        Iterator<Result> iterator = scanner.iterator();
+        LinkedList<String> keys = new LinkedList<>();
+        while (iterator.hasNext()) {
+            String key = "";
+            Result result = iterator.next();
+            for (Cell cell : result.rawCells()) {
+                byte[] rowkey = CellUtil.cloneRow(cell);
+                byte[] family = CellUtil.cloneFamily(cell);
+                byte[] column = CellUtil.cloneQualifier(cell);
+                byte[] value = CellUtil.cloneValue(cell);
+                key = Bytes.toString(rowkey) + ":" + Bytes.toString(family) + ":" + Bytes.toString(column);
+                keys.add(key);
+            }
+        }
+        System.out.println(keys);
+        scanner.close();
+        table.close();
+        connection.close();
     }
 }
