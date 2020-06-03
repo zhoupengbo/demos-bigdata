103149b496c7a7a9fffd5026c1fbe5b239e0d962
zhou.pengbo <zhou.pengbo@xcar.com.cn>
2020-06-03 17:36:08 +0800

    Update ColumnPaginationFilterDemo.java

--- hbase/hbase-filters-demos/src/main/java/com/zpb/demos/ColumnPaginationFilterDemo.java
+++ hbase/hbase-filters-demos/src/main/java/com/zpb/demos/ColumnPaginationFilterDemo.java
@@ -1,4 +1,61 @@
 package com.zpb.demos;
 
+import com.zpb.utils.MyBase;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.ColumnCountGetFilter;
+import org.apache.hadoop.hbase.filter.ColumnPaginationFilter;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * 宽行读取过滤条件，适用于filter。宽表分页读取列。
+ * 基于ColumnCountGetFilter的过滤器采用两个参数：limit和offset.
+ * 该过滤器可用于基于行的索引，其中对其他表的引用存储在许多列中，以便为最终用户进行有效的查找和分页结果. 仅将最新版本视为分页.
+ */
 public class ColumnPaginationFilterDemo {
+    private static boolean isok = false;
+    private static String tableName = "test";
+    private static String[] cfs = new String[]{"f1","f2"};
+    private static String[] data = new String[]{"row-1:f1:c1:v1", "row-1:f1:c2:v2", "row-1:f2:c3:v3", "row-1:f2:c4:v4"};
+    public static void main(String[] args) throws IOException {
+
+        MyBase myBase = new MyBase();
+        Connection connection = myBase.createConnection();
+        if (isok) {
+            myBase.deleteTable(connection, tableName);
+            myBase.createTable(connection, tableName, cfs);
+            // 造数据
+            myBase.putRows(connection, tableName, data);
+        }
+        Table table = connection.getTable(TableName.valueOf(tableName));
+        Scan scan = new Scan();
+//        ColumnPaginationFilter columnPaginationFilter = new ColumnPaginationFilter(1,0); // [row-1:f1:c1]
+        ColumnPaginationFilter columnPaginationFilter = new ColumnPaginationFilter(2,1); // [row-1:f1:c2, row-1:f2:c3]
+        scan.setFilter(columnPaginationFilter);
+        ResultScanner scanner = table.getScanner(scan);
+        Iterator<Result> iterator = scanner.iterator();
+        LinkedList<String> keys = new LinkedList<>();
+        while (iterator.hasNext()) {
+            String key = "";
+            Result result = iterator.next();
+            for (Cell cell : result.rawCells()) {
+                byte[] rowkey = CellUtil.cloneRow(cell);
+                byte[] family = CellUtil.cloneFamily(cell);
+                byte[] column = CellUtil.cloneQualifier(cell);
+                byte[] value = CellUtil.cloneValue(cell);
+                key = Bytes.toString(rowkey) + ":" + Bytes.toString(family) + ":" + Bytes.toString(column);
+                keys.add(key);
+            }
+        }
+        System.out.println(keys);
+        scanner.close();
+        table.close();
+        connection.close();
+    }
 }
